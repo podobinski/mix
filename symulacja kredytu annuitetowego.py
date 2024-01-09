3529f782ec30ddf51168114c82389e31f68335da
podobinski <piotr.podobinski@gmail.com>
2024-01-09 18:49:53 +0100

    Simplification
    removing unused functionality.

--- symulacja kredytu annuitetowego.py
+++ symulacja kredytu annuitetowego.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import datetime
 from decimal import Decimal, ROUND_HALF_UP
-from openpyxl import Workbook
+# Removed the unnecessary import of openpyxl.Workbook
 
 def ostatni_dzien_miesiaca(data):
     nastepny_miesiac = data.replace(day=28) + datetime.timedelta(days=4)
